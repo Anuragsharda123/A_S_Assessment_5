c47c354f654b6976593c48abedd8d3cd6f97078f
Anuragsharda123 <anuragsharda131@gmail.com>
2024-10-17 23:37:46 +0530

    commit

--- backend/src/model/userModel.ts
+++ backend/src/model/userModel.ts
@@ -2,17 +2,17 @@ import { Model, DataTypes } from "sequelize";
 import sequelize from "../config/db"
 
 
-
 class User extends Model {
     public id?: number;
-    public firstname!:string;
-    public lastname!:string;
+    public firstname!: string;
+    public lastname!: string;
     public email!: string;
     public password!: string;
-    public phone!:string;
-    public gender!:number;
-    public profile_photo!:string;
-    public resume!:string; 
+    public is_approved!: number
+    public phone!: string;
+    public gender!: number;
+    public profile_photo!: string;
+    public resume!: string; 
     public user_type!: number;
     public is_active!: boolean;
     public AgencyId!: number
@@ -27,6 +27,7 @@ User.init({
     phone:{type:DataTypes.STRING, allowNull:false, unique:true},
     gender:{type:DataTypes.INTEGER, allowNull:false},
     profile_photo:{type:DataTypes.STRING, allowNull:false, unique:true},
+    is_approved:  { type: DataTypes.INTEGER, allowNull: false, defaultValue:2 },
     resume: {type:DataTypes.STRING, allowNull:true},
     is_active: {type: DataTypes.BOOLEAN, defaultValue:false},
     user_type: { type: DataTypes.INTEGER, allowNull:false, defaultValue:1 }
